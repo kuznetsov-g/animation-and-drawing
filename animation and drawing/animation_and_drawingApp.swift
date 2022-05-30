f5e11ff6019615242da61ad3c17ff06415842ee1
George <backs159@icloud.com>
2022-05-30 19:09:18 +0500

    Initial Commit

---
+++ animation and drawing/animation_and_drawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  animation_and_drawingApp.swift
+//  animation and drawing
+//
+//  Created by Георгий Кузнецов on 30.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct animation_and_drawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
